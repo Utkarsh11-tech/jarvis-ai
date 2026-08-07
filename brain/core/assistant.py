4ee50569a44f83956321782cbdba65bf1a49c8ea
Utkarsh11-tech <utkarshsaraswat11@gmail.com>
2026-08-07 15:08:19 +0530

    added some functions in main

---
+++ brain/core/assistant.py
@@ -0,0 +1,10 @@
+class Assistant:
+    def __init__(self):
+        print("Assistant created")
+
+    def start(self)  : 
+        print("Jarvis is now online")
+
+    def initialize(self) : 
+        print("Initializing all required modules.....") 
+        
--- brain/main.py
+++ brain/main.py
@@ -1,3 +1,11 @@
-def main () : 
-    print("hello world")
-main()
+from core.assistant import Assistant
+
+
+def main():
+    jarvis = Assistant()
+    jarvis.start()
+    jarvis.initialize()
+
+
+if __name__ == "__main__":
+    main()
