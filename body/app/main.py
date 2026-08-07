8adcbd6a5d44e6616b2716ac12df09e833c213d5
Sagar <sagar.j.korade@gmail.com>
2026-08-07 18:44:03 +0530

    Add first UI components

--- body/app/main.py
+++ body/app/main.py
@@ -1,7 +1,14 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QLabel,
+    QVBoxLayout,
+)
 
+from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,6 +16,18 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("JARVIS")
         self.resize(1000, 650)
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.main_layout = QVBoxLayout()
+
+        self.central_widget.setLayout(self.main_layout)
+
+        title = QLabel("JARVIS AI")
+        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addWidget(title)
 
 
 app = QApplication(sys.argv)
