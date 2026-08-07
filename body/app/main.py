56a19824023e3f7bf464e588f96b9af3a339513c
Sagar <sagar.j.korade@gmail.com>
2026-08-07 23:48:13 +0530

    changed the structure of body folders

--- body/app/main.py
+++ body/app/main.py
@@ -1,38 +1,18 @@
 import sys
 
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QWidget,
-    QLabel,
-    QVBoxLayout,
-)
+from PySide6.QtWidgets import QApplication
 
-from PySide6.QtCore import Qt
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("JARVIS")
-        self.resize(1000, 650)
-
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.main_layout = QVBoxLayout()
-
-        self.central_widget.setLayout(self.main_layout)
-
-        title = QLabel("JARVIS AI")
-        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.main_layout.addWidget(title)
+from screens.home.home_screen import MainWindow
 
 
-app = QApplication(sys.argv)
+def main():
+    app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+    window = MainWindow()
+    window.show()
 
-sys.exit(app.exec())
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
---
+++ body/app/screens/home/home_screen.py
@@ -0,0 +1,31 @@
+from PySide6.QtCore import Qt
+
+from PySide6.QtWidgets import (
+    QLabel,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("JARVIS")
+        self.resize(1000, 650)
+
+        # Central Widget
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        # Main Layout
+        self.main_layout = QVBoxLayout()
+
+        self.central_widget.setLayout(self.main_layout)
+
+        # Title
+        self.title = QLabel("JARVIS AI")
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addWidget(self.title)
