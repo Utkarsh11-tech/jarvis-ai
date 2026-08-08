ae957823fd90aedc6ddf3a1f3fdb51fa5f34216e
Sagar <sagar.j.korade@gmail.com>
2026-08-08 13:03:29 +0530

    main.py created

--- body/app/widgets/orb.py
+++ body/app/widgets/orb.py
@@ -15,7 +15,16 @@ class OrbWidget(QWidget):
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
         painter.setBrush(QBrush(QColor("#00D9FF")))
-
         painter.setPen(Qt.PenStyle.NoPen)
 
-        painter.drawEllipse(50, 50, 200, 200)
+        center_x = self.width() // 2
+        center_y = self.height() // 2
+
+        radius = min(self.width(),self.height()) // 4 
+
+        painter.drawEllipse(
+            center_x - radius,
+            center_y - radius,
+            radius * 2,
+            radius * 2
+        )
