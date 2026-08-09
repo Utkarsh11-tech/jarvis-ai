a60cd1ba6c49e1fe5f7ca7973632d5d730869362
Utkarsh11-tech <utkarshsaraswat11@gmail.com>
2026-08-09 13:41:38 +0530

    Create voice_worker.py

---
+++ brain/voices/voice_worker.py
@@ -0,0 +1,54 @@
+from PySide6.QtCore import QObject, Signal
+
+from brain.voices.listener import VoiceListener
+
+
+class VoiceWorker(QObject):
+
+    command_received = Signal(str)
+    finished = Signal()
+
+    def __init__(self):
+        super().__init__()
+
+        self.listener = None
+        self.running = True
+
+    def run(self):
+        """
+        Starts the voice listening loop.
+        """
+
+        self.listener = VoiceListener()
+
+        print("JARVIS Voice Worker is ready.")
+
+        try:
+
+            while self.running:
+
+                awakened = self.listener.listen_for_wake_word()
+
+                if not awakened:
+                    continue
+
+                command = self.listener.listen_for_command()
+
+                if command:
+                    self.command_received.emit(command)
+
+                self.listener.prepare_for_wake_word()
+
+        finally:
+
+            if self.listener:
+                self.listener.close()
+
+            self.finished.emit()
+
+    def stop(self):
+        """
+        Stops the voice worker.
+        """
+
+        self.running = False
