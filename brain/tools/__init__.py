78a6e7a4ac36218984a2ada3e4cf80771ff82f65
Utkarsh11-tech <utkarshsaraswat11@gmail.com>
2026-08-13 13:51:42 +0530

    Add JARVIS tool package

---
+++ brain/tools/__init__.py
@@ -0,0 +1,9 @@
+"""JARVIS tool registry and tool definitions."""
+
+from brain.tools.registry import ToolDefinition, ToolRegistry, get_default_registry
+
+__all__ = [
+    "ToolDefinition",
+    "ToolRegistry",
+    "get_default_registry",
+]
